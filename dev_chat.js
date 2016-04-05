7913b542935db285cc8a02c139a5d2f0bcc079f7
Paul Scarrone <ninjapanzer@gmail.com>
2016-04-05 10:14:40 -0400

    with faye

--- dev_chat.js
+++ dev_chat.js
@@ -1,9 +1,18 @@
 var chatWindow = document.getElementsByClassName("chat-window")[0];
 var inputBox = document.getElementsByClassName("message-input")[0];
 
+var client = new Faye.Client('http://localhost:8000/');
+
+client.subscribe('/messages', function(message) {
+  console.log('Got a message: ' + message.text);
+});
+
 var sendMessage = function(){
-	var currentMessages = chatWindow.innerHTML;
+	//var currentMessages = chatWindow.innerHTML;
 	var inputMessage = inputBox.value;
-	var appendedMessage = currentMessages + "<br/>" + inputMessage;
-	chatWindow.innerHTML = appendedMessage;
+	client.publish('/messages', {
+  		text: inputMessage
+	});
+	//var appendedMessage = currentMessages + "<br/>" + inputMessage;
+	//chatWindow.innerHTML = appendedMessage;
 }
