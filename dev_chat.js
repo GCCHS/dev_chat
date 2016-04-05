61f72ed518af1f790455a1659823a474225e119a
Paul Scarrone <ninjapanzer@gmail.com>
2016-04-05 10:47:58 -0400

    IP based

--- dev_chat.js
+++ dev_chat.js
@@ -3,16 +3,23 @@ var inputBox = document.getElementsByClassName("message-input")[0];
 
 var client = new Faye.Client('http://localhost:8000/');
 
+jQuery(inputBox).on("keyup", function(event){
+	if(event.keyCode === 13){
+		sendMessage();
+	}
+})
+
 client.subscribe('/messages', function(message) {
+  var currentMessages = chatWindow.innerHTML;
+  var appendedMessage = message.text + "<br/>" + currentMessages;
+  chatWindow.innerHTML = appendedMessage;
   console.log('Got a message: ' + message.text);
 });
 
 var sendMessage = function(){
-	//var currentMessages = chatWindow.innerHTML;
 	var inputMessage = inputBox.value;
 	client.publish('/messages', {
   		text: inputMessage
 	});
-	//var appendedMessage = currentMessages + "<br/>" + inputMessage;
-	//chatWindow.innerHTML = appendedMessage;
+	inputBox.value = '';
 }
